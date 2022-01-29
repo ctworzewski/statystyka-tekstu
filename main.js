0f20c143f9b25333c872cfaafa3c8aad43ac4969
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-01-30 00:55:04 +0100

    Add test.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const textarea = document.querySelector('textarea');
 const button = document.querySelector('button');
-
 const arr = [];
+const characeterTest = document.querySelector('.characeterTest');
+const wordsTest = document.querySelector('.wordssTest');
+
 button.addEventListener('click', function() {
     const addSigns = textarea.value;
     //console.log('Przycisk działa');
@@ -18,5 +20,28 @@ button.addEventListener('click', function() {
     textarea.value = '';
     console.log('tablica', newTab);
     console.log('tablica arr', arr);
+
+    function connectWords() {
+        const connectCharacters = arr.join(',');
+        console.log(connectCharacters);
+    }
+    connectWords();
+
+    const allCharacters = arr.reduce( (acc,val) => {
+        let sum = acc + val;
+        return sum;
+    })
+    console.log('suma znaków to: ', allCharacters);
+
+    let ileZnakow = addSigns.length;
+    characeterTest.innerHTML = ileZnakow;
+
+    function howWords() {
+        if (textarea.value == ' ') {
+            console.log('to jest pojedyncze słowo')
+            wordsTest.innerHTML;
+        }
+    }
+    howWords();
     
 });
---
+++ test.js
@@ -0,0 +1,17 @@
+const textarea = document.querySelector('.textarea');
+
+
+
+textarea.addEventListener('input', onChnage);
+
+function onChnage(e) {
+    
+    const tekst = textarea.value;
+    const ileZnakow = tekst.length;
+    const ileSlow = tekst.split(' ').length;
+    const ileZdan = tekst.split('.').length;
+
+    document.getElementById('ileZnakow').innerHTML = ileZnakow;
+    document.getElementById('ileSlow').innerHTML = ileSlow;
+    document.getElementById('ileZdan').innerHTML = ileZdan;
+}
